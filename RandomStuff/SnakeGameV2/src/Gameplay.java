5620eaad5ce697007b2a1359e07a9ddc13739461
BugzTheBunny <44586585+BugzTheBunny@users.noreply.github.com>
2019-06-01 19:45:32 +0300

    Realised movement

--- RandomStuff/SnakeGameV2/src/Gameplay.java
+++ RandomStuff/SnakeGameV2/src/Gameplay.java
@@ -22,7 +22,7 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 	private int lenthOfSnake = 3;
 	private ImageIcon rightMount, downMount, upMount, leftMount, snakeImage, titleImage;
 	private Timer timer;
-	private int delay = 100;
+	private int delay = 25;
 
 	public Gameplay() {
 		addKeyListener(this);
@@ -34,12 +34,12 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 	}
 
 	public void paint(Graphics graphics) {
-		
-		if(moves == 0) {
+
+		if (moves == 0) {
 			snakexlength[2] = 50;
 			snakexlength[1] = 75;
 			snakexlength[0] = 100;
-			
+
 			snakeylength[2] = 100;
 			snakeylength[1] = 100;
 			snakeylength[0] = 100;
@@ -77,8 +77,8 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 				upMount = new ImageIcon("upmouth.png");
 				upMount.paintIcon(this, graphics, snakexlength[i], snakeylength[i]);
 			}
-			
-			if(i != 0) {
+
+			if (i != 0) {
 				snakeImage = new ImageIcon("snakeimage.png");
 				snakeImage.paintIcon(this, graphics, snakexlength[i], snakeylength[i]);
 			}
@@ -88,24 +88,139 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		timer.start();
+		if (right) {
+			for (int r = lenthOfSnake - 1; r >= 0; r--) {
+				snakeylength[r + 1] = snakeylength[r];
+			}
+			for (int r = lenthOfSnake; r >= 0; r--) {
+				if (r == 0) {
+					snakexlength[r] = snakexlength[r] + 25;
+				} else {
+					snakexlength[r] = snakexlength[r - 1];
+				}
+				if (snakexlength[r] > 850) {
+					snakexlength[r] = 25;
+				}
+
+			}
+			repaint();
+		}
+		if (left) {
+			for (int r = lenthOfSnake - 1; r >= 0; r--) {
+				snakeylength[r + 1] = snakeylength[r];
+			}
+			for (int r = lenthOfSnake; r >= 0; r--) {
+				if (r == 0) {
+					snakexlength[r] = snakexlength[r] - 25;
+				} else {
+					snakexlength[r] = snakexlength[r - 1];
+				}
+				if (snakexlength[r] < 25) {
+					snakexlength[r] = 850;
+				}
+
+			}
+			repaint();
+
+		}
+		if (up) {
+			for (int r = lenthOfSnake - 1; r >= 0; r--) {
+				snakexlength[r + 1] = snakexlength[r];
+			}
+			for (int r = lenthOfSnake; r >= 0; r--) {
+				if (r == 0) {
+					snakeylength[r] = snakeylength[r] - 25;
+				} else {
+					snakeylength[r] = snakeylength[r - 1];
+				}
+				if (snakeylength[r] < 75) {
+					snakeylength[r] = 625;
+				}
+
+			}
+			repaint();
+
+		}
+		if (down) {
+			for (int r = lenthOfSnake - 1; r >= 0; r--) {
+				snakexlength[r + 1] = snakexlength[r];
+			}
+			for (int r = lenthOfSnake; r >= 0; r--) {
+				if (r == 0) {
+					snakeylength[r] = snakeylength[r] + 25;
+				} else {
+					snakeylength[r] = snakeylength[r - 1];
+				}
+				if (snakeylength[r] > 625) {
+					snakeylength[r] = 75;
+				}
+
+			}
+			repaint();
+		}
 
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
+	public void keyPressed(KeyEvent k) {
 
 	}
 
 	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-
+	public void keyReleased(KeyEvent k) {
+		if (k.getKeyCode() == KeyEvent.VK_RIGHT) {
+			moves++;
+			right = true;
+			if (!left) {
+				right = true;
+			} else {
+				right = false;
+				left = true;
+			}
+			up = false;
+			down = false;
+		}
+		if (k.getKeyCode() == KeyEvent.VK_LEFT) {
+			moves++;
+			left = true;
+			if (!right) {
+				left = true;
+			} else {
+				left = false;
+				right = true;
+			}
+			up = false;
+			down = false;
+		}
+		if (k.getKeyCode() == KeyEvent.VK_UP) {
+			moves++;
+			up = true;
+			if (!down) {
+				up = true;
+			} else {
+				up = false;
+				down = true;
+			}
+			left = false;
+			right = false;
+		}
+		if (k.getKeyCode() == KeyEvent.VK_DOWN) {
+			moves++;
+			down = true;
+			if (!up) {
+				down = true;
+			} else {
+				down = false;
+				up = true;
+			}
+			left = false;
+			right = false;
+		}
 	}
 
 	@Override
-	public void keyTyped(KeyEvent arg0) {
+	public void keyTyped(KeyEvent k) {
 		// TODO Auto-generated method stub
 
 	}
